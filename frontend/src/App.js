3c8161f929a383fbae370209cb010fc5f80f6c6d
Donald Ballowe <dballowe7912@gmail.com>
2022-05-24 05:33:11 -0500

    Added deleteTask

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import FilterButton from './components/FilterButton'
 const App = (props) => {
 
   const [ data, setData ] = useState([])
+  const [ dataLength, setDataLength ] = useState(data.length)
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/tasks')
@@ -16,21 +17,37 @@ const App = (props) => {
     .catch(function (error) {
       console.log(error)
     })
-  }, [])
+  },[dataLength])
+
+  const toggleTaskCompleted = (id) => {
+    console.log(data[0])
+  }
+
+  const deleteTask = async (id) => {
+    await axios.delete(`http://localhost:5000/api/tasks/${id}`).then(setDataLength(dataLength - 1))
+  }
+
 
   const tasksList = data.map((task) => (
     <Todo
-      name={task.name} 
-      completed={task.completed} 
-      id={task.id} 
-      key={task.id}
+      name={task.name}
+      completed={task.completed}
+      id={task._id}
+      key={task._id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
-    )
-  )
+  ))
 
-  const addTask = (name) => {
-    console.log(name)
+  const addTask = async (name) => {
+    const newTask = {name}
+    await axios.post("http://localhost:5000/api/tasks", newTask)
+    setDataLength(dataLength + 1)
   }
+
+  const taskCount = data.length
+  const tasksNoun = taskCount !== 1 ? 'tasks' : 'task'
+  const headingText = `${taskCount} ${tasksNoun} remaining`
 
   return (
     <div className="todoapp stack-large">
@@ -41,7 +58,7 @@ const App = (props) => {
         <FilterButton key='active' name='Active'/>
         <FilterButton key='completed' name='Completed'/>
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
